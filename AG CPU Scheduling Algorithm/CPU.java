daeb27b1b3f91ce162477cb4fe669e4848c04495
Mariam Ahmad <36169438+mariam-beshary9@users.noreply.github.com>
2020-04-14 23:14:59 +0200

    Create CPU.java

---
+++ AG CPU Scheduling Algorithm/CPU.java
@@ -0,0 +1,26 @@
+
+package cpu;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Mariam Ahmed Amin
+ */
+public class CPU {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      //  while (true) {
+            
+           
+                AG_Handler handler = new AG_Handler();
+                handler.run();
+            
+            
+    //  }
+    }
+}
